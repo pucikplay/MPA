1a410fc9a41675962bc9d4aa002764c5167f1e70
Gabriel <pucikplay@gmail.com>
2023-12-18 12:27:30 +0100

    New distribution

--- L4/utils.jl
+++ L4/utils.jl
@@ -31,6 +31,7 @@ function _closestPair(points::Array{Point})
     setL = [p for p in points if p.y <= medianY]
     setR = [p for p in points if p.y > medianY]
 
+    # Recursively find minimal distances in halves
     dL, cmpL = _closestPair(setL)
     dR, cmpR = _closestPair(setR)
     d = min(dL, dR)
@@ -74,6 +75,11 @@ end
 
 d = Truncated(Normal(0, 1), 0, 1)
 function truncNormal()
+    return rand(d)
+end
+
+d = Truncated(Normal(0.5, 0.5), 0, 1)
+function truncNormalBump()
     return rand(d)
 end
 
