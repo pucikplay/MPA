9cfdfe6e0b3a552bced064345acee4d397b0095f
Gabriel <pucikplay@gmail.com>
2024-01-17 16:57:55 +0100

    LogSer distribution written

--- L5/logSer.jl
+++ L5/logSer.jl
@@ -1,66 +1,98 @@
+using SpecialFunctions
 using Distributions
-import Base.rand, StatsBase.params
-import Random, Distributions, Statistics, StatsBase
+using Random
+import Random.rand
+import SpecialFunctions.beta_inc
+import Distributions.@check_args, Distributions.@distr_support, Distributions.DiscreteUnivariateDistribution
 
 struct LogSer{T<:Real} <: DiscreteUnivariateDistribution
-    p::T ## p parameter
-    function LogSer{T}(p::T; check_args = true) where {T<:Real}
-        check_args && Distributions.@check_args(LogSer, p>0 && 1>p)
-        return new{T}(p)
+    p::T
+    function LogSer{T}(p::T) where {T <: Real}
+        new{T}(p)
     end
 end
 
-# constructor for no type and param Float64
-function LogSer(p::Float64; check_args = true)
-    return LogSer{Float64}(p, check_args = check_args)
+function LogSer(p::Real; check_args::Bool=true)
+    @check_args LogSer (p, zero(p) < p < one(p))
+    return LogSer{typeof(p)}(p)
 end
 
-# 0 helper
-StatsBase.params(d::LogSer) = (d.p,)
+LogSer() = LogSer{Float64}(0.5)
 
-# rand(::AbstractRNG, d::UnivariateDistribution)
-function Base.rand(rng::AbstractRNG, d::LogSer)
-    p = StatsBase.params(d)
+@distr_support LogSer 1 Inf
 
-    r = log1p(-p)
+### Conversions
+Distributions.convert(::Type{LogSer{T}}, p::Real) where {T<:Real} = LogSer(T(p))
+Base.convert(::Type{LogSer{T}}, d::LogSer) where {T<:Real} = LogSer{T}(T(d.p))
+Base.convert(::Type{LogSer{T}}, d::LogSer{T}) where {T<:Real} = d
+
+### Parameters
+Distributions.params(d::LogSer) = (d.p,)
+Distributions.partype(::LogSer{T}) where {T<:Real} = T
+
+
+### Statistics
+
+Distributions.mean(d::LogSer) = (-d.p)/(log1p(-d.p) * (1 - d.p))
+
+Distributions.mode(d::LogSer{T}) where {T<:Real} = one(T)
+
+Distributions.var(d::LogSer) = (d.p^2 + d.p * log1p(-d.p)) / ((1 - d.p)^2 * (log1p(-d.p))^2)
+
+### Evaluations
+
+function Distributions.pdf(d::LogSer, x::Int)
+    insupport(d, x) ? -(d.p^x) / (log1p(-d.p) * x) : zero(d.p)
+end
+
+function Distributions.logpdf(d::LogSer, x::Real)
+    insupport(d, x) ? log(pdf(d,Int(x))) : log(zero(d.p))
+end
+
+function Distributions.cdf(d::LogSer, x::Real)
+    pdf_d(a) = pdf(d,a)
+    insupport(d, x) ? sum(pdf_d, 1:x) : zero(d.p)
+end
+
+function Distributions.quantile(d::LogSer, p::Real)
+    k = 1
+    cdf_k = pdf(d,k)
+    while cdf_k < p
+        k += 1
+        cdf_k += pdf(d,k)
+    end
+    return k
+end
+
+### Sampling
+
+function Random.rand(rng::AbstractRNG, d::LogSer)
+    r = log1p(-d.p)
     while true
-        V = rand(rnd)
-        if V >= p
+        V = Random.rand(rng)
+
+        if V >= d.p
             return 1
         end
-        U = rand(rng)
+
+        U = Random.rand(rng)
         q = -expm1(r * U)
-        if V <= q*q
-            result = floor(1 + log(V) / log(q))
+
+        if V <= q * q
+            result = Int(floor(1 + log(V) / log(q)))
             if (result < 1) || (V == 0.0)
                 continue
             else
                 return result
             end
         end
+
         if V >= q
             return 1
         end
+
         return 2
     end
 end
-# sampler(d::Distribution)
-Distributions.sampler(rng::AbstractRNG, d::LogSeq) = Base.rand(rng::AbstractRNG, d::LogSer)
 
-# logpdf(d::UnivariateDistribution, x::Real)
-function Distributions.pdf(d::LogSer{T}, x::Real) where {T<:Real}
-    p = params(d)
-    return (-p^k)/(k*log(1-p))
-end
-
-Distributions.logpdf(d::LogSer, k::Integer) = log(pdf(d,k))
-
-# cdf(d::UnivariateDistribution, x::Real)
-function Distributions.cdf(d::LogSer)
-    
-end
-
-# quantile(d::UnivariateDistribution, q::Real)
-# minimum(d::UnivariateDistribution)
-# maximum(d::UnivariateDistribution)
-# insupport(d::UnivariateDistribution, x::Real)
+sampler(rng::AbstractRNG, d::LogSer) = rand(rng::AbstractRNG, d::LogSer)
